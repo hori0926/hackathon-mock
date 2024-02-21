40d1e5f2c38a5fc581f1d9d235bccb08407db7a5
hori0926 <hirotaka.h09@gmail.com>
2024-02-22 02:01:46 +0900

    mysql

--- mock/settings.py
+++ mock/settings.py
@@ -78,10 +78,17 @@ WSGI_APPLICATION = 'mock.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'mock',
+        "USER": 'django',
+        "HOST": 'localhost',
+        "PASSWORD": '',
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4",
+        },
+    },
 }
 
 
